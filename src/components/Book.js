c65392db8b7a519d7f06d4d174b743d7590c089e
mule <belmeetmule@gmail.com>
2022-09-24 01:40:50 +0300

    update style

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,21 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const { id, title, author } = props;
   return (
     <li>
-      <h2>
+      <h2 style={{ marginBottom: 0, paddingBottom: 0 }}>
+        {' '}
         {title}
         {' '}
-        by
-        {author}
       </h2>
-      <button type="button">Remove</button>
+      <p style={{ margin: 0, padding: 0 }}>
+        {' '}
+        {author}
+        {' '}
+      </p>
+      <button id={id} type="button">Remove</button>
     </li>
   );
 };
 
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
