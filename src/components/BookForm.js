a29f5180bfa3822fcf1e1bdcdbee80ecc6e7c685
mule <belmeetmule@gmail.com>
2022-10-04 23:45:49 +0300

    add functionalities to the addNewBook button

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const BookForm = () => (
-  <>
-    <h3 style={{ marginTop: '2rem' }}>ADD NEW BOOK</h3>
-    <form style={{ display: 'flex', gap: '2rem' }}>
-      <input type="text" placeholder="Title" />
-      <input type="text" placeholder="Author" />
-      <button type="button">Add Book</button>
-    </form>
-  </>
-);
+const BookForm = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    const { target: { name, value } } = e;
+    if (name === 'title') setTitle(value);
+    else if (name === 'author') setAuthor(value);
+  };
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+
+    // Sets back the input fields
+    setAuthor('');
+    setTitle('');
+  };
+
+  return (
+    <>
+      <h3 style={{ marginTop: '2rem' }}>ADD NEW BOOK</h3>
+      <form onSubmit={handleSumbit} style={{ display: 'flex', gap: '2rem' }}>
+        <input
+          type="text"
+          value={title}
+          placeholder="Title"
+          name="title"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          value={author}
+          placeholder="Author"
+          name="author"
+          onChange={handleChange}
+        />
+        <button type="submit">Add Book</button>
+      </form>
+    </>
+  );
+};
 
 export default BookForm;
