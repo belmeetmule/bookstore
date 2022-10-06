01770f47f68a6230501ac2de1969ac7b312f27d1
mule <belmeetmule@gmail.com>
2022-10-07 00:47:11 +0300

    add features to adding newbook and remove book to/from the api

--- src/api.js
+++ src/api.js
@@ -2,6 +2,20 @@ const appId = 'LpS0hST1DeFrx6DWsQyI';
 const baseURL = `https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${appId}/books/`;
 
 const api = {
+  // get books from the endpoint
+  fetchBooks: async () => {
+    const response = await fetch(baseURL);
+    const data = await response.json();
+
+    const books = Object.entries(data)
+      .map(([id, book]) => {
+        const { title, author } = book[0];
+        return { id, title, author };
+      })
+      .sort((a, b) => a.title.localeCompare(b.title));
+
+    return books;
+  },
 
   // Add new book to the endpoint
   addNewBook: async ({
@@ -40,19 +54,6 @@ const api = {
       .then((res) => {
         if (res.status !== 201) { console.log('delete operation is not successfull'); }
       });
-  },
-  fetchBooks: async () => {
-    const response = await fetch(baseURL);
-    const data = await response.json();
-
-    const books = Object.entries(data)
-      .map(([id, book]) => {
-        const { title, author } = book[0];
-        return { id, title, author };
-      })
-      .sort((a, b) => a.title.localeCompare(b.title));
-
-    return books;
   },
 };
 
