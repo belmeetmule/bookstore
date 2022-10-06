c12d9ffb79f07b69ebc924aaf2a0c5b989c237ee
mule <belmeetmule@gmail.com>
2022-10-07 00:50:20 +0300

    fetch and display books data from the api

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import BooksList from './BookList';
 import BookForm from './BookForm';
+import { getBooks } from '../redux/books/books';
 
 const BookContainer = () => {
   // get data from react state
   const books = useSelector((state) => state.books);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getBooks());
+  }, []);
 
   return (
     <>
